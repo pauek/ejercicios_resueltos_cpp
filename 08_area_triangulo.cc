6ba23549cb6aa63616bb0dc21d71edddcb3af4e0
Pau Fernández <pau.fernandez@upc.edu>
2011-03-22 10:52:46 +0100

    Area del triangulo

---
+++ 08_area_triangulo.cc
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+double area_triangulo(double base, double altura) {
+  return base * altura / 2.0;
+}
+
+int main() {
+  int b, h;
+  cout << "Base? ";   cin >> b;
+  cout << "Altura? "; cin >> h;
+  cout << "Area: " << area_triangulo(b, h) << endl;
+}
